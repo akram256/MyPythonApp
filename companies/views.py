28896f7aade677270ddbb9632c62112b3254f885
akrammukasa <akram@bposeats.com>
2021-04-06 18:08:31 +0300

    work on update delete compaines

--- companies/views.py
+++ companies/views.py
@@ -36,3 +36,30 @@ class CompanyView(ListAPIView):
         serializer.save()
         return Response({"message":"Company has been  successfully added"},
                         status=status.HTTP_201_CREATED)
+
+class CompanyRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    permission_classes =(AllowAny,)
+    serializer_class = CompanySerializer
+    lookup_field = 'id'
+    queryset=Company.objects.all()
+    
+
+    def get_object(self):
+        return get_object_or_404(
+            self.get_queryset(), id=self.kwargs.get('id'))
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data,
+                                         partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({'message':'Successfully updated',
+            'data':serializer.data},status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"message": " Company has been successfully deleted"},
+                        status=status.HTTP_200_OK)
